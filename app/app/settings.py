c1315d992690e328afdf021074386fabd24460b0
Al Ardosa <alardosa@gmail.com>
2022-04-04 08:21:14 +0800

    Update 04042022 8:20

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '+*bg^10as!01b(w15ny1+5vc%r)e6nta9h6#rdppfodzy7n#=#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
